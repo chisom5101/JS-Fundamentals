6aff45a495cf828bbfe14f2a1a2274022511d3c1
chisom5101 <chisomisiguzor94@gmail.com>
2025-05-21 23:17:29 +0100

    4 concat

---
+++ 4-concat.js
@@ -0,0 +1,5 @@
+// 4-concat.js
+const firstWord = process.argv[2] || "undefined"; // First word (or "undefined")
+const secondWord = process.argv[3] || "undefined"; // Second word (or "undefined")
+
+console.log(${firstWord} is ${secondWord});
