4043e0b3712bc47ab9080c96236f4ab84ce1e728
Chisom <chisomisiguzor94@gmail.com>
2025-05-22 00:09:27 +0100

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,11 +1,5 @@
 // 4-concat.js
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
+const arg1 = process.argv[2] ?? "undefined";
+const arg2 = process.argv[3] ?? "undefined";
 
-if (arg1 && arg2) {
-  console.log(`${arg1} ${arg2}`);  // Two arguments: joined by space
-} else if (arg1) {
-  console.log(arg1);  // One argument: print as-is
-} else {
-  console.log();  // No arguments: print nothing (empty line)
-}
+console.log(`${arg1} is ${arg2}`);
